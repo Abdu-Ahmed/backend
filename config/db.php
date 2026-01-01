69397efcf52e67e4d87c8a3093d4f55f5b5a953b
Abdu Ahmed <iamabduahmed@gmail.com>
2026-01-01 12:39:11 +0200

    edit: modified config/db to work with RW creds

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'host' => getenv('DB_HOST') ?: '127.0.0.1',
+    'host' => getenv('DB_HOST') ?: 'mysql.railway.internal',
     'port' => getenv('DB_PORT') ?: '3306',
-    'database' => getenv('DB_NAME') ?: 'assignment',
+    'database' => getenv('DB_NAME') ?: 'railway',
     'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
+    'password' => getenv('DB_PASSWORD') ?: 'UlqoiYvsJzCPrwBjIwITlDtyJcCbCgSz',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
 ];
